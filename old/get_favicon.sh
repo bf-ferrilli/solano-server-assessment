301d73675f5d8781c15edfffdc5d1a071250e25a
Brandon Froehlich <bfroehlich@ferrilli.com>
2025-03-14 17:11:08 -0500

    Fixed tagging for role and os columns, and added support for multiple envs/roles.

--- old/get_favicon.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Fetch the page, extract the favicon URL, then download it
-favicon_url=$(curl -s "https://welcome.solano.edu/" | grep -oP '(?<=<img id="preview-favicon" src=")[^"]+')
-[ -n "$favicon_url" ] && curl -O "$favicon_url"
